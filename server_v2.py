4108be1f3f83c2b9c61c11c6595b1a6a24f971c4
iKiway <105075696+iKiway@users.noreply.github.com>
2024-04-21 00:19:41 +0200

    Update server_v2.py

--- server_v2.py
+++ server_v2.py
@@ -1,7 +1,7 @@
 import socket
 import threading
 
-HOST = '192.168.178.156'  # Standard loopback interface address (localhost)
+HOST = 'sdf'  # Standard loopback interface address (localhost)
 PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
 
 try:
