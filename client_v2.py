0d2490d8249abfb9ccceedcfb07af213dca4b201
iKiway <105075696+iKiway@users.noreply.github.com>
2024-05-02 20:02:21 +0200

    Update client_v2.py

--- client_v2.py
+++ client_v2.py
@@ -1,7 +1,7 @@
 import socket
 import time
 
-HOST = 'localhost'  # The server's hostname or IP address
+HOST = 'dronepi'  # The server's hostname or IP address
 PORT = 65432        # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
