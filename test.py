2c3436a6722c956e130b295477cf11fa46820811
iKiway <105075696+iKiway@users.noreply.github.com>
2024-07-12 00:13:14 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,3 @@
 import subprocess
-ls_output = subprocess.check_output(f"ted.py", universal_newlines=True)
+ls_output = subprocess.check_output(f"python ted.py", universal_newlines=True)
 print(ls_output)
