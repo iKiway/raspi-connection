3bb7a557c3f5b4d396d656c1c624861069f1e3f0
iKiway <105075696+iKiway@users.noreply.github.com>
2024-07-12 00:27:09 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import subprocess
 import os
 
-a = subprocess.run("sudo raspi-config nonint do_wifi_ssid_passphrase test test [hidden] [plain] 2>&1", capture_output=True, text=True, shell=True)
+a = subprocess.run("sudo raspi-config nonint do_wifi_ssid_passphrase test test [hidden] [plain]", capture_output=True, text=True, shell=True)
 print(a)
