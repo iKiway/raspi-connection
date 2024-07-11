7faa26f09f8dd45a53f2101880a751d79f612d73
iKiway <105075696+iKiway@users.noreply.github.com>
2024-07-12 00:16:14 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
 import subprocess
 import os
-ls_output = os.system(f"sudo raspi-config nonint do_wifi_ssid_passphrase sdfas asdfasdf [hidden] [plain] 2>&1", universal_newlines=True)
+ls_output = os.system(f"sudo raspi-config nonint do_wifi_ssid_passphrase sdfas asdfasdf [hidden] [plain] 2>&1")
 print(ls_output)
