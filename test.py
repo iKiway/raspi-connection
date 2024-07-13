919bb826250285a038e6aad7a20e3af9e40c2a78
iKiway <105075696+iKiway@users.noreply.github.com>
2024-07-13 22:21:49 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import subprocess
 import os
 
-a = subprocess.run("sudo raspi-config nonint do_wifi_ssid_passphrase FRITZles\!Box 08416737145150857484 [hidden] [plain]", capture_output=True, text=True, shell=True)
+a = subprocess.run("sudo raspi-config nonint do_wifi_ssid_passphrase FRITZles!Box 08416737145150857484 [hidden] [plain]", capture_output=True, text=True, shell=True)
 print(a)
